4b1583ef2c933c45a8b1489aec90abbef67205ad
Frank Jung <frankhjung@linux.com>
2026-02-19 16:29:47 +1100

    Add car inventory and discount functionality with tests

---
+++ test/tutorial/chapter4_test.clj
@@ -0,0 +1,28 @@
+(ns tutorial.chapter4-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [tutorial.chapter4 :refer [cars affordable-cars, DISCOUNT10, DISCOUNT20]]))
+
+(deftest valid-coupon-test
+  (testing "valid coupon returns correct cars"
+    (let [budget 50000
+          coupon DISCOUNT20
+          affordable (affordable-cars cars budget coupon)]
+      (is (= 2 (count affordable)))
+      (is (some #(= "BMW" (:type %)) affordable))
+      (is (some #(= "FIAT" (:type %)) affordable))))
+
+  (testing "valid coupon with strict budget (avoid double discount bug)"
+    (let [budget 45000
+          coupon DISCOUNT20
+          affordable (affordable-cars cars budget coupon)]
+      (is (= 1 (count affordable))) ;; Only FIAT (20000 * 0.8 = 16000) should be affordable. BMW (60000 * 0.8 = 48000) is > 45000.
+      (is (some #(= "FIAT" (:type %)) affordable))
+      (is (not (some #(= "BMW" (:type %)) affordable))))))
+
+(deftest invalid-coupon-test
+  (testing "invalid coupon returns correct cars"
+    (let [budget 50000
+          coupon DISCOUNT10 ;; Invalid coupon, no discount applied
+          affordable (affordable-cars cars budget coupon)]
+      (is (= 1 (count affordable)))
+      (is (some #(= "FIAT" (:type %)) affordable)))))
